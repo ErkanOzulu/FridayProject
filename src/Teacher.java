7a1a8afc88ddb6d52f99308f1496c69cde77b3f8
Alp <calptekincevik@gmail.com>
2022-12-16 21:54:52 +0300

    alp created teacher

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,4 +1,8 @@
-public class Teacher  {
+public class Teacher extends Employee {
+
+    public Teacher(String name, int age, int id, char gender) {
+        super(name, age, id, gender);
+    }
 
     public void teach(){
         System.out.println(" is teaching.");
